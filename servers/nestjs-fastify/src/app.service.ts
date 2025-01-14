52ff7d354ddfec5c53974c55dfaf8e33e00a2120
ivanlappo <ivan@leaptheory.com>
2025-01-14 09:14:14 -0800

    added nestjs-fastify project

---
+++ servers/nestjs-fastify/src/app.service.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async getHttpClient(link: string): Promise<string> {
+    if (!link) {
+      throw new BadRequestException("Query parameter 'link' is required");
+    }
+    let url = null;
+
+    try {
+      url = new URL(link);
+    } catch {
+      throw new BadRequestException("Invalid 'link' URL");
+    }
+
+    try {
+      const response = await fetch(url);
+      return await response.text();
+    } catch (err) {
+      throw new BadRequestException(err);
+    }
+  }
+}
